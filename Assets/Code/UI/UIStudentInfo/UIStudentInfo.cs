b35142299be88799aaabeb4e254e21100823117f
FH <976047167@qq.com>
2017-07-06 08:03:33 +0800

    弟子信息的UI

---
+++ Assets/Code/UI/UIStudentInfo/UIStudentInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using KEngine.UI;
+using KSFramework;
+
+
+
+
+
+public class UIStudentInfo : CSUIController  {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void OnInit()
+    {
+        Debug.LogError("OnInit");
+    }
+
+    public override void OnOpen(params object[] args)
+    {
+        Debug.LogError("OnOpen");
+    }
+
+    public override void OnClose()
+    {
+        Debug.LogError("OnOpen");
+    }
+}
