34cc986e8f8d517b5d3cfcb430fb90b6425767fb
FH <976047167@qq.com>
2017-09-19 03:35:52 +0800

    登录UI，制作中

---
+++ Assets/Code/UI/UILogin1/UILogin1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using KSFramework;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UILogin1 : MonoBehaviour {
+    private string serverListUrl="http://47.94.220.1/serverlist.html";
+    public Button btnLogin;
+    public Button btnForgetPwd;
+    public Button btnRegister;
+
+
+    // Use this for initialization
+    void Start () {
+        btnLogin.onClick.RemoveAllListeners();
+        btnLogin.onClick.AddListener(Login_LoginServer);
+        btnForgetPwd.onClick.RemoveAllListeners();
+        btnForgetPwd.onClick.AddListener(forgetPwd);
+        btnRegister.onClick.RemoveAllListeners();
+        btnRegister.onClick.AddListener(forgetPwd);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void Login_LoginServer(){
+
+
+
+
+    }
+    void forgetPwd() {
+
+
+    }
+
+    void toRegisterStep (){
+
+    }
+
+}   
