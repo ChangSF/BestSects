dd4ac535ce5eba6ad51b9fa8a2736d754cdecd6a
FH <976047167@qq.com>
2017-09-21 01:52:36 +0800

    no message

--- Assets/Code/UI/UILogin1/UI_Login1.cs
+++ Assets/Code/UI/UILogin1/UI_Login1.cs
@@ -3,12 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using KSFramework;
 using UnityEngine.UI;
-
+using LitJson;
+using BestSects.protocol;
+using BestSects.Net;
+using KEngine.UI;
 
 public class UI_Login1 : CSUIController
 {
-    private Button btnRegister, btinLogin, btnForgetPwd, btnSignIn, btnSignUP;
+    private Button btnRegister, btnLogin, btnForgetPwd, btnSignIn, btnSignUP;
     private Canvas objRegister, objLogin, objServers;
+    private InputField inputID, inputPassword;
+
+    private string token;
+    private string username;
+    private Dictionary<string,string> loginServer;
+    private List<Serverlist> serverlist;
+    private struct Serverlist {
+        public string server;
+        public string ip;
+        public string port;
+        public string name;
+
+    }
+    private bool bGetServerList = false;
+
+
 
 
     public override void OnInit()
@@ -16,14 +35,15 @@ public class UI_Login1 : CSUIController
      UILogic<UI_Login1>.Instance.UIReg(this);
         Debug.LogError(GetType().ToString() + ":OnInit");
         btnRegister = transform.Find("imgBackground/objLogin/imgGroupRegister/btnRegister").GetComponent<Button>();
-        btinLogin = transform.Find("imgBackground/objLogin/imgGroupLogin/btnLogin").GetComponent<Button>();
+        btnLogin = transform.Find("imgBackground/objLogin/imgGroupLogin/btnLogin").GetComponent<Button>();
         btnForgetPwd =transform.Find("imgBackground/objLogin/imgGroupLogin/btnForgetPwd").GetComponent<Button>();
         btnSignIn = transform.Find("imgBackground/objRegister/imgGroupRegister/btnSignIn").GetComponent<Button>();
-        btnSignUP = transform.Find("imgBackground/objServers/imgGroupHaveLogined/btnSignUP").GetComponent<Button>();
+        btnSignUP = transform.Find("imgBackground/objServers/imgGroupHaveLogined/btnSignUp").GetComponent<Button>();
         objRegister = transform.Find("imgBackground/objRegister").GetComponent<Canvas>();
         objLogin = transform.Find("imgBackground/objLogin").GetComponent<Canvas>();
         objServers = transform.Find("imgBackground/objServers").GetComponent<Canvas>();
-
+        inputID= transform.Find("imgBackground/objLogin/imgGroupLogin").GetComponent<InputField>();
+        inputPassword= transform.Find("imgBackground/objLogin/imgGroupLogin").GetComponent<InputField>();
 
         btnForgetPwd.onClick.RemoveAllListeners();
         btnForgetPwd.onClick.AddListener(ForgetPwd);
@@ -31,11 +51,78 @@ public class UI_Login1 : CSUIController
         btnRegister.onClick.RemoveAllListeners();
         btnRegister.onClick.AddListener(ToRegisterStep);
 
+        btnLogin.onClick.RemoveAllListeners();
+        btnLogin.onClick.AddListener(() => 
+        {
+            if (!bGetServerList)
+            {
+                return;
+            }
+            Login_LoginServer();
+
+        });
     }
 
     public override void OnOpen(params object[] args)
     {
         Debug.LogError(GetType().ToString() + ":OnOpen");
+        Debug.LogError("================================ WND_Login:OnOpen ============================");
+        Messenger.AddListener("NetworkConnect", () =>
+       {
+           Debug.LogError("连接成功");
+           LoginServer();
+       });
+        Messenger.AddListener("CurrentPlayerInfo", (JsonData a) =>
+        {
+
+            if (a["Code"].ToString().Equals("0"))
+            {
+                Cookie.Set("CurrentPlayerInfo", a["PlayerInfo"]);
+                UIModule.Instance.CloseWindow("WND_Login1");
+                UIModule.Instance.OpenWindow("WND_Introduce", "user1");
+
+            }
+        });
+
+            object serverListJson = Cookie.Get("serverListJson", () =>
+            {
+                Debug.LogError("无法获取到服务器列表");
+                return null;
+            });
+        Debug.LogError(serverListJson.ToString());
+
+        JsonReader reader = new JsonReader(serverListJson.ToString());
+
+        JsonData data = new JsonData();
+        data =  JsonMapper.ToObject(reader);
+
+        loginServer = new Dictionary<string, string>();
+        JsonData LoginServerData = data["login"];
+        loginServer.Add("ip", LoginServerData["ip"].ToString());
+        loginServer.Add("port", LoginServerData["port"].ToString());
+
+        List<string> gameServerList = new List<string>();
+        JsonData serverListData = data["game"];
+        int x;
+        if (!int.TryParse(serverListData["Count"].ToString(),out x))
+        {
+            x = 0;
+        }
+
+        for (int i=0;i<x; i++)
+        {
+            JsonData item = serverListData[i];
+            Serverlist j = new Serverlist();
+            j.server = item["server"].ToString();
+            j.ip = item["ip"].ToString();
+            j.port = item["port"].ToString();
+            j.name = item["name"].ToString();
+            serverlist.Insert(i,j );
+            Debug.LogError(item["ip"].ToString());
+            bGetServerList = true;
+        }
+
+
     }
 
     public override void OnClose()
@@ -44,8 +131,36 @@ public class UI_Login1 : CSUIController
         Debug.LogError(GetType().ToString() + ":OnClose");
     }
 
+
+    public void Login_LoginServer()
+    {
+        if (inputID.text == "" || inputPassword.text=="")
+        {
+
+
+
+        }
+
+
+    }
+
+
+
+
     public void ForgetPwd()
     {
+
+
+    }
+    public void LoginServer()
+    {
+        ReqLoginMessage msg = new ReqLoginMessage();
+        Debug.LogError("z => " + token);
+        msg.Token = token;
+        msg.Username = this.username;
+        msg.Channel = 0; //玩家渠道
+        msg.DeviceId = "abcdef"; //设备号
+        NetworkModule.Instance.Send(MessageID.ReqLogin, msg);
 
 
     }
@@ -55,7 +170,6 @@ public class UI_Login1 : CSUIController
         objLogin.enabled = false;
         objRegister.enabled = true;
         objServers.enabled = false;
-
 
     }
 
--- Assets/Code/UI/UILogin1/UI_Login2.cs
+++ Assets/Code/UI/UILogin1/UI_Login2.cs
@@ -10,17 +10,28 @@ public class UI_Login2 : MonoBehaviour {
     // Use this for initialization
     void Start () {
         btnRegister = transform.Find("imgBackground/objLogin/imgGroupRegister/btnRegister").GetComponent<Button>();
-        btinLogin = transform.Find("btinLogin").GetComponent<Button>();
-        btnForgetPwd = transform.Find("btnForgetPwd").GetComponent<Button>();
-        btnSignIn = transform.Find("btnSignIn").GetComponent<Button>();
-        btnSignUP = transform.Find("btnSignUP").GetComponent<Button>();
-        objRegister = transform.Find("objRegister").GetComponent<Canvas>();
-        objLogin = transform.Find("objLogin").GetComponent<Canvas>();
-        objServers = transform.Find("objServers").GetComponent<Canvas>();
+        btinLogin = transform.Find("imgBackground/objLogin/imgGroupLogin/btnLogin").GetComponent<Button>();
+        btnForgetPwd = transform.Find("imgBackground/objLogin/imgGroupLogin/btnForgetPwd").GetComponent<Button>();
+        btnSignIn = transform.Find("imgBackground/objRegister/imgGroupRegister/btnSignIn").GetComponent<Button>();
+        btnSignUP = transform.Find("imgBackground/objServes/imgGroupHaveLogined/btnSignUp").GetComponent<Button>();
+        objRegister = transform.Find("imgBackground/objRegister").GetComponent<Canvas>();
+        objLogin = transform.Find("imgBackground/objLogin").GetComponent<Canvas>();
+        objServers = transform.Find("imgBackground/objServers").GetComponent<Canvas>();
+        btnRegister.onClick.RemoveAllListeners();
+        btnRegister.onClick.AddListener(ToRegisterStep);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
+
+    public void ToRegisterStep()
+    {
+        objLogin.enabled = false;
+        objRegister.enabled = true;
+        objServers.enabled = false;
+
+
+    }
 }
