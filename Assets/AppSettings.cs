6bedce4ca45f8881b687fd527b361fc7e90c093b
CSF <952327293@qq.com>
2018-01-03 23:05:28 +0800

    啊啊啊

--- Assets/AppSettings.cs
+++ Assets/AppSettings.cs
@@ -50,6 +50,7 @@ namespace AppSettings
                     { 
                         AudioConfigSettings._instance,
                         ErrorCodeSettings._instance,
+                        SkillConfigSettings._instance,
                         TestSettings._instance,
                         GameConfigSettings._instance,
                     };
@@ -499,6 +500,223 @@ namespace AppSettings
         public static string ParsePrimaryKey(TableFileRow row)
         {
             var primaryKey = row.Get_string(row.Values[0], "");
+            return primaryKey;
+        }
+	}
+
+	/// <summary>
+	/// Auto Generate for Tab File: "SkillConfig.k"
+    /// No use of generic and reflection, for better performance,  less IL code generating
+	/// </summary>>
+    public partial class SkillConfigSettings : IReloadableSettings
+    {
+        /// <summary>
+        /// How many reload function load?
+        /// </summary>>
+        public static int ReloadCount { get; private set; }
+
+		public static readonly string[] TabFilePaths = 
+        {
+            "SkillConfig.k"
+        };
+        internal static SkillConfigSettings _instance = new SkillConfigSettings();
+        Dictionary<int, SkillConfigSetting> _dict = new Dictionary<int, SkillConfigSetting>();
+
+        /// <summary>
+        /// Trigger delegate when reload the Settings
+        /// </summary>>
+	    public static System.Action OnReload;
+
+        /// <summary>
+        /// Constructor, just reload(init)
+        /// When Unity Editor mode, will watch the file modification and auto reload
+        /// </summary>
+	    private SkillConfigSettings()
+	    {
+        }
+
+        /// <summary>
+        /// Get the singleton
+        /// </summary>
+        /// <returns></returns>
+	    public static SkillConfigSettings GetInstance()
+	    {
+            if (ReloadCount == 0)
+            {
+                _instance._ReloadAll(true);
+    #if UNITY_EDITOR
+                if (SettingModule.IsFileSystemMode)
+                {
+                    for (var j = 0; j < TabFilePaths.Length; j++)
+                    {
+                        var tabFilePath = TabFilePaths[j];
+                        SettingModule.WatchSetting(tabFilePath, (path) =>
+                        {
+                            if (path.Replace("\\", "/").EndsWith(path))
+                            {
+                                _instance.ReloadAll();
+                                Log.LogConsole_MultiThread("File Watcher! Reload success! -> " + path);
+                            }
+                        });
+                    }
+
+                }
+    #endif
+            }
+
+	        return _instance;
+	    }
+        
+        public int Count
+        {
+            get
+            {
+                return _dict.Count;
+            }
+        }
+
+        /// <summary>
+        /// Do reload the setting file: SkillConfig, no exception when duplicate primary key
+        /// </summary>
+        public void ReloadAll()
+        {
+            _ReloadAll(false);
+        }
+
+        /// <summary>
+        /// Do reload the setting class : SkillConfig, no exception when duplicate primary key, use custom string content
+        /// </summary>
+        public void ReloadAllWithString(string context)
+        {
+            _ReloadAll(false, context);
+        }
+
+        /// <summary>
+        /// Do reload the setting file: SkillConfig
+        /// </summary>
+	    void _ReloadAll(bool throwWhenDuplicatePrimaryKey, string customContent = null)
+        {
+            for (var j = 0; j < TabFilePaths.Length; j++)
+            {
+                var tabFilePath = TabFilePaths[j];
+                TableFile tableFile;
+                if (customContent == null)
+                    tableFile = SettingModule.Get(tabFilePath, false);
+                else
+                    tableFile = TableFile.LoadFromString(customContent);
+
+                using (tableFile)
+                {
+                    foreach (var row in tableFile)
+                    {
+                        var pk = SkillConfigSetting.ParsePrimaryKey(row);
+                        SkillConfigSetting setting;
+                        if (!_dict.TryGetValue(pk, out setting))
+                        {
+                            setting = new SkillConfigSetting(row);
+                            _dict[setting.Id] = setting;
+                        }
+                        else 
+                        {
+                            if (throwWhenDuplicatePrimaryKey) throw new System.Exception(string.Format("DuplicateKey, Class: {0}, File: {1}, Key: {2}", this.GetType().Name, tabFilePath, pk));
+                            else setting.Reload(row);
+                        }
+                    }
+                }
+            }
+
+	        if (OnReload != null)
+	        {
+	            OnReload();
+	        }
+
+            ReloadCount++;
+            Log.Info("Reload settings: {0}, Row Count: {1}, Reload Count: {2}", GetType(), Count, ReloadCount);
+        }
+
+	    /// <summary>
+        /// foreachable enumerable: SkillConfig
+        /// </summary>
+        public static IEnumerable GetAll()
+        {
+            foreach (var row in GetInstance()._dict.Values)
+            {
+                yield return row;
+            }
+        }
+
+        /// <summary>
+        /// GetEnumerator for `MoveNext`: SkillConfig
+        /// </summary> 
+	    public static IEnumerator GetEnumerator()
+	    {
+	        return GetInstance()._dict.Values.GetEnumerator();
+	    }
+         
+	    /// <summary>
+        /// Get class by primary key: SkillConfig
+        /// </summary>
+        public static SkillConfigSetting Get(int primaryKey)
+        {
+            SkillConfigSetting setting;
+            if (GetInstance()._dict.TryGetValue(primaryKey, out setting)) return setting;
+            return null;
+        }
+
+        // ========= CustomExtraString begin ===========
+        
+        // ========= CustomExtraString end ===========
+    }
+
+	/// <summary>
+	/// Auto Generate for Tab File: "SkillConfig.k"
+    /// Singleton class for less memory use
+	/// </summary>
+	public partial class SkillConfigSetting : TableRowFieldParser
+	{
+		
+        /// <summary>
+        /// ID Column/编号/主键
+        /// </summary>
+        public int Id { get; private set;}
+        
+        /// <summary>
+        /// Name/名字
+        /// </summary>
+        public string Name { get; private set;}
+        
+        /// <summary>
+        /// 1为子弹类型技能
+        /// </summary>
+        public int SkillType { get; private set;}
+        
+        /// <summary>
+        /// CommentB井号注释列
+        /// </summary>
+        public float FlySpeed { get; private set;}
+        
+
+        internal SkillConfigSetting(TableFileRow row)
+        {
+            Reload(row);
+        }
+
+        internal void Reload(TableFileRow row)
+        { 
+            Id = row.Get_int(row.Values[0], ""); 
+            Name = row.Get_string(row.Values[1], ""); 
+            SkillType = row.Get_int(row.Values[2], ""); 
+            FlySpeed = row.Get_float(row.Values[3], ""); 
+        }
+
+        /// <summary>
+        /// Get PrimaryKey from a table row
+        /// </summary>
+        /// <param name="row"></param>
+        /// <returns></returns>
+        public static int ParsePrimaryKey(TableFileRow row)
+        {
+            var primaryKey = row.Get_int(row.Values[0], "");
             return primaryKey;
         }
 	}
